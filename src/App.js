1b416983100df3114da6b7440dc6cc2d2396243c
Chenqian-t <1473828153@qq.com>
2021-08-04 16:13:54 +0800

    count by redux

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
 import './reset.css';
-import Frame from './components/Frame';
+import { connect } from 'react-redux';
+import { add, jian } from './actions';
 
-export default class App extends Component {
+const mapStateToProps = state => {
+	return {
+		count: state.count.count,
+	}
+}
+
+// const mapDispatchToProps = dispatch => {
+// 	return {
+// 		add: () => {
+// 			dispatch(add())
+// 		},
+// 		jian: () => {
+// 			dispatch(jian())
+// 		}
+// 	}
+// }
+
+class App extends Component {
+	increment = () => {
+		this.props.add();
+	}
+	decrement = () => {
+		this.props.jian();
+	}
 	render() {
 		return (
 			<div className='app'>
-				<Frame />
+				<button onClick={this.decrement}>-</button>
+				{this.props.count}
+				<button onClick={this.increment}>+</button>
 			</div>
 		)
 	}
 }
+
+export default connect(mapStateToProps, { add, jian })(App)
---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+
+const add = () => ({
+    type: 'ADD',
+});
+
+const jian = () => ({
+    type: 'JIAN',
+});
+
+export {add, jian}
