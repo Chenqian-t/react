388215498d3de54b3db53a5a171e9e62d317feaf
Chenqian-t <1473828153@qq.com>
2021-08-10 15:23:49 +0800

    hooks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App012';
+import App from './App013';
 import { BrowserRouter } from "react-router-dom";
 import Scroll from './components/Scroll';
 import { Provider } from 'react-redux';
